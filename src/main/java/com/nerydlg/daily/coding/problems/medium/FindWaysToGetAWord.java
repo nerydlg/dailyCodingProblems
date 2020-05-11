cefa66159ab6db648b878f6f986c135906f3cff0
Nery Delgado <ne.delgado@gmail.com>
2020-05-11 17:51:16 -0500

    fixed how many ways

--- src/main/java/com/nerydlg/daily/coding/problems/medium/FindWaysToGetAWord.java
+++ src/main/java/com/nerydlg/daily/coding/problems/medium/FindWaysToGetAWord.java
@@ -1,52 +1,41 @@
 package com.nerydlg.daily.coding.problems.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindWaysToGetAWord {
 
+    private Map<Integer, Integer> memo = new HashMap<>();
+
     public int howManyWaysToGetAWord(String numeric) {
-        return howManyWaysToGetAWord(numeric, 1);
+        if(numeric == null || numeric.length() == 0) {
+            return 0;
+        }
+        return howManyWaysToGetAWord(0, numeric);
     }
 
-//    public int howManyWaysToGetAWord(String numeric, int modifier) {
-//        int last = 27;
-//
-//        if(numeric.length() == 1){
-//            return numeric.length();
-//        } else if(numeric.length() == 2){
-//            int num = Integer.valueOf(numeric);
-//            if(num == 0) {
-//                return 0;
-//            } if(num >= last) {
-//                return 1 + modifier;
-//            } else {
-//                return 2 + modifier;
-//            }
-//        } else {
-//            return howManyWaysToGetAWord(numeric.substring(0,2), -1) +
-//                    howManyWaysToGetAWord(numeric.substring(1), 0);
-//        }
-//    }
-
-    public int howManyWaysToGetAWord(String numeric, int previous) {
-        int last = 27;
-        if(previous == 0) return 0;
-        if(numeric.length() == 1){
-            if(numeric.equals("0")) {
-                return 0;
-            } else {
-                return previous;
-            }
-        } else if(numeric.length() == 2){
-            int num = Integer.valueOf(numeric);
-            if(num == 0 || (numeric.matches("0?[0-9]0?") && num >= last)) {
-                return 0;
-            } if(num >= last || numeric.matches("0?[0-9]0?")) {
-                return previous;
-            } else {
-                return previous + 1;
-            }
-        } else {
-            int prev = howManyWaysToGetAWord(numeric.substring(0,2), previous);
-            return howManyWaysToGetAWord(numeric.substring(1), prev);
+    public int howManyWaysToGetAWord(int index, String numeric) {
+        if(index == numeric.length()) {
+            return 1;
         }
+
+        if(numeric.charAt(index) == '0') {
+            return 0;
+        }
+
+        if(index == numeric.length() - 1) {
+            return 1;
+        }
+
+        if(memo.containsKey(index)) {
+            return memo.get(index);
+        }
+
+        int result = howManyWaysToGetAWord(index + 1, numeric);
+        if(Integer.valueOf(numeric.substring(index, index+2)) <= 26) {
+            result += howManyWaysToGetAWord(index+2, numeric);
+        }
+        memo.put(index, result);
+        return result;
     }
 }
