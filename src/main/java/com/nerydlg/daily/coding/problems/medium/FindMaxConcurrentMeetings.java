c3365e34d563688d80e68795248f071079ea7f1e
Nery Delgado <ne.delgado@gmail.com>
2022-02-24 23:58:44 -0600

    remove empty files

--- src/main/java/com/nerydlg/daily/coding/problems/medium/FindMaxConcurrentMeetings.java
+++
@@ -1,2 +0,0 @@
-package com.nerydlg.daily.coding.problems.medium;public class FindMaxConcurrentMeetings {
-}
--- src/test/java/com/nerydlg/daily/coding/problems/medium/FindMaxConcurrentMeetingsTest.java
+++
@@ -1,6 +0,0 @@
-package com.nerydlg.daily.coding.problems.medium;
-
-import static org.junit.jupiter.api.Assertions.*;
-class FindMaxConcurrentMeetingsTest {
-  
-}
